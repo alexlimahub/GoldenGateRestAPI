44bf3875e56da2406076b2bd4e9b304b68dfead2
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-24 08:59:15 -0500

    Delete kill_reverse_replicat.sh

--- kill_reverse_replicat.sh
+++
@@ -1,6 +0,0 @@
-#-- KILL REPLICAT
-curl -s -K /Users/alexlima/Documents/LABS/access.cfg \
--H "Content-Type: application/json" \
--H "Accept: application/json" \
--d '{"status": "killed"}' \
--X PATCH https://j4w5mi5njtqa.deployment.goldengate.us-ashburn-1.oci.oraclecloud.com/services/v2/replicats/RDEMOR|  python -m json.tool 
