8da350074d8dd4dee4aaaee8358a2e44448cf20a
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-24 09:16:25 -0500

    Update lag_all_processes.sh

--- lag_all_processes.sh
+++ lag_all_processes.sh
@@ -1,7 +1,7 @@
 #-- LAG of all processes
 curl -s -K /Users/alexlima/Documents/LABS/access.cfg \
 -H "Accept: application/json" \
--X POST https://j4w5mi5njtqa.deployment.goldengate.us-ashburn-1.oci.oraclecloud.com/services/v2/commands/execute \
+-X POST https://<IP or hostname>/services/v2/commands/execute \
 -d '{
     "name":"report",
     "reportType":"lag",
