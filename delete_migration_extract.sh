6ebedec7a9c21122408b9dc8e52103076c5a5e83
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-24 09:12:15 -0500

    Update delete_migration_extract.sh

--- delete_migration_extract.sh
+++ delete_migration_extract.sh
@@ -2,6 +2,6 @@
 curl -s -K /Users/alexlima/Documents/LABS/access.cfg \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
--X DELETE https://j4w5mi5njtqa.deployment.goldengate.us-ashburn-1.oci.oraclecloud.com/services/v2/extracts/EDEMO \
+-X DELETE https://<IP or hostname>/services/v2/extracts/<Extract Name> \
 --data-raw '' \
 |  python -m json.tool 
