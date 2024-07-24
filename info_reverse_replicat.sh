41b167c9cb0cc93d28152c1ffeda57ceb414726a
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-24 08:58:55 -0500

    Delete info_reverse_replicat.sh

--- info_reverse_replicat.sh
+++
@@ -1,5 +0,0 @@
-curl -s -K /Users/alexlima/Documents/LABS/access.cfg \
--H "Content-Type: application/json" \
--H "Accept: application/json" \
--X GET https://j4w5mi5njtqa.deployment.goldengate.us-ashburn-1.oci.oraclecloud.com/services/v2/replicats/RDEMOR/info/status  \
-| jq '.response'
