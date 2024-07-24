6bb68c3420bfa8b056ccc2e09a68dfc6d274bf85
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-24 09:16:05 -0500

    Update kill_migration_replicat.sh

--- kill_migration_replicat.sh
+++ kill_migration_replicat.sh
@@ -3,4 +3,4 @@ curl -s -K /Users/alexlima/Documents/LABS/access.cfg \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
 -d '{"status": "killed"}' \
--X PATCH https://j4w5mi5njtqa.deployment.goldengate.us-ashburn-1.oci.oraclecloud.com/services/v2/replicats/RDEMO|  python -m json.tool 
+-X PATCH https://<Ip or hostname>/services/v2/replicats/<replicat name>|  python -m json.tool 
