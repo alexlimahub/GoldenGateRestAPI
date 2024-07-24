b783008af232a2a46d1c15d8325dc299248eac99
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-24 08:58:18 -0500

    Delete delete_reverse_extract.sh

--- delete_reverse_extract.sh
+++
@@ -1,5 +0,0 @@
-#-- DELETE EXTRACT
-curl -s -K /Users/alexlima/Documents/LABS/access.cfg \
--H "Content-Type: application/json" \
--H "Accept: application/json" \
--X DELETE https://j4w5mi5njtqa.deployment.goldengate.us-ashburn-1.oci.oraclecloud.com/services/v2/extracts/EDEMOR|  python -m json.tool 
