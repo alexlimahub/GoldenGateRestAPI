70b076bd1e64e2c65712fb23389f61ba256d28e0
Alex Lima <77405808+alexlimahub@users.noreply.github.com>
2024-07-25 15:55:04 -0500

    Update retrive_services_ports.sh

--- retrive_services_ports.sh
+++ retrive_services_ports.sh
@@ -1,4 +1,4 @@
 #-- Thic will check the Services PORTS
 
 curl -k  -L 'https://<URL>/services/v2/installation/services' \
--H 'Authorization: Basic <encoded username:password.' | jq '.response' | egrep 'port|serviceName' | uniq
+-H 'Authorization: Basic <encoded username:password>' | jq '.response' | egrep 'port|serviceName' | uniq
