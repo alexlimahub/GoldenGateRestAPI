c4376cb3c49baa3c9c0ff82d3431e301b1bce349
Alex Lima <alex.l.lima@oracle.com>
2025-11-17 14:29:26 -0600

    Update change_session_timeout.sh

--- change_session_timeout.sh
+++ change_session_timeout.sh
@@ -1,4 +1,5 @@
 #-- This example allow you to modify the WebUI session timeout settings
+#-- Author: Alex Lima
 #-- Documentation for this options:
 #-- https://docs.oracle.com/en/middleware/goldengate/core/23/oggra/op-services-version-deployments-deployment-services-service-patch.html
 
